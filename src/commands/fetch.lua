bb46b776979c0abeb4dde25d99a5bf0204c28a66
SJoshua <JoshuaSRKF@gmail.com>
2020-11-17 03:58:25 +0800

    feat: command fetch

---
+++ src/commands/fetch.lua
@@ -0,0 +1,34 @@
+local fetch = {
+    func = function(msg)
+        local f = io.popen("git pull", "r")
+        local s = f:read("*a")
+        f:close()
+
+        if (s:find("Already up to date")) then
+            bot.sendMessage{
+                chat_id = msg.chat.id,
+                reply_to_message_id = msg.message_id,
+                text = "Already up to date."
+            }
+        else 
+            local f = io.popen("git log -1", "r")
+            local s = f:read("*a")
+            f:close()
+
+            bot.sendMessage{
+                chat_id = msg.chat.id,
+                reply_to_message_id = msg.message_id,
+                text = "*[latest commit]*```\n" .. s .. "\n```",
+                parse_mode = "Markdown"
+            }
+
+            return commands.reload.func(msg)
+        end
+    end,
+    desc = "fetch latest code.",
+    limit = {
+        master = true
+    }
+}
+
+return fetch
