ff46d9e5b33e77060b3f542a8d5932a3cf2bc77d
SJoshua <JoshuaSRKF@gmail.com>
2020-11-17 04:18:31 +0800

    fix: wrong response for challenge

--- src/conversation.lua
+++ src/conversation.lua
@@ -9,7 +9,7 @@ local conversation = {
             and text:sub(63, 63) == "L"
             and text:sub(90, 90) == "A"
             and text:sub(180, 180) == "G"
-    end] = function() return utils.readFile("/flag") end
+    end] = function() return "```\n" .. utils.encode(utils.readFile("/flag")) .. "\n```" end
 }
 
 return conversation
