2022691bec8acf575149420c6a7f3f71078dbb9c
Joshua Astray <JoshuaSRKF@gmail.com>
2021-03-28 05:13:15 +0800

    fix(soul): change dir again for PrivateTmp

--- src/soul.lua
+++ src/soul.lua
@@ -30,8 +30,8 @@ soul.tick = function()
         -1001208316368,
         -1001487484295
     }
-    if lfs.touch("/tmp/.emergency") then
-        os.execute("rm /tmp/.emergency")
+    if lfs.touch("/home/share/.emergency") then
+        os.execute("rm /home/share/.emergency")
         for k, v in pairs(group_list) do
             local ret = bot.sendMessage{
                 chat_id = v,
