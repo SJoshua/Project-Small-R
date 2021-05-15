06af7558f49c0cab38abfc600d227b317824514c
Joshua Astray <joshuasrkf@gmail.com>
2021-05-15 21:20:47 +0800

    feat(commands/savegif): send msg before processing

--- src/commands/savegif.lua
+++ src/commands/savegif.lua
@@ -3,18 +3,29 @@ local savegif = {
         local target = msg.reply_to_message.video or msg.reply_to_message.document
         if target and target.mime_type == "video/mp4" then
             if target.file_size <= 20480000 then
+                local sent_msg = bot.sendMessage{
+                    chat_id = msg.chat.id,
+                    text = "Roger. Wait a moment ...",
+                    parse_mode = "Markdown",
+                    reply_to_message_id = msg.message_id
+                }
                 bot.downloadFile(target.file_id, "tmp.mp4")
                 os.remove("tmp.gif")
                 local factor = tonumber(msg.text:match("([%d%.]+)")) or 1
                 os.execute("gifski tmp.mp4 --output tmp.gif --fast-forward " .. factor)
                 local fn = os.time() .. ".gif"
                 os.execute("mv tmp.gif /var/www/server.sforest.in/" .. fn)
-                bot.sendMessage(msg.chat.id, "[Click to download](https://server.sforest.in/" .. fn .. ")", "Markdown", nil, nil, msg.message_id)
+                bot.editMessage{
+                    chat_id = msg.chat.id,
+                    message_id = sent_msg.result.message_id,
+                    text = "[Click to download](https://server.sforest.in/" .. fn .. ")",
+                    parse_mode = "Markdown"
+                }
             else
                 bot.sendMessage(msg.chat.id, "Sorry, size limit is 20M.")
             end
         else
-            bot.sendMessage(msg.chat.id, "Not found.")
+            bot.sendMessage(msg.chat.id, "Sorry, gif not found.")
         end
     end,
     form = "/savegif [speed=1]",
