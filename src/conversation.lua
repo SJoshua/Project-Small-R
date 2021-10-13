90709f74f5a5db82aad91e14973d3908f84553f5
Joshua Astray <JoshuaSRKF@gmail.com>
2021-10-13 11:22:51 +0800

    feat: remove time-limited flag

--- src/conversation.lua
+++ src/conversation.lua
@@ -3,14 +3,7 @@ local utils = require("utils")
 local conversation = {
     ["^%s*ping%s*$"] = "pong!",
     ["^%s*乒%s*$"] = "乓！",
-    ["1%D-1%D-4%D-5%D-1%D-4"] = "sticker#CAADBQADLAAD1vXIAYjCdJop7aEIAg",
-    [function(text)
-        return #text == 233
-            and text:sub(79, 79) == "F"
-            and text:sub(63, 63) == "L"
-            and text:sub(90, 90) == "A"
-            and text:sub(180, 180) == "G"
-    end] = function() return "```\n" .. utils.encode(utils.readFile("/flag")) .. "\n```" end
+    ["1%D-1%D-4%D-5%D-1%D-4"] = "sticker#CAADBQADLAAD1vXIAYjCdJop7aEIAg"
 }
 
 return conversation
