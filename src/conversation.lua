5dedf83d267a1e946f47455cd65e2769930dd97a
Joshua Astray <JoshuaSRKF@gmail.com>
2022-02-26 18:39:18 +0800

    feat: change trigger of sticker '114514'

--- src/conversation.lua
+++ src/conversation.lua
@@ -3,7 +3,7 @@ local utils = require("utils")
 local conversation = {
     ["^%s*ping%s*$"] = "pong!",
     ["^%s*乒%s*$"] = "乓！",
-    ["1%D-1%D-4%D-5%D-1%D-4"] = "sticker#CAADBQADLAAD1vXIAYjCdJop7aEIAg"
+    ["1%s-1%s-4%s-5%s-1%s-4"] = "sticker#CAADBQADLAAD1vXIAYjCdJop7aEIAg"
 }
 
 return conversation
