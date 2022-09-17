eec393d1f84dde9115f5046bbaf15e59c58af190
Joshua Astray <JoshuaSRKF@gmail.com>
2022-09-17 17:23:51 +0800

    chore: redact 2 triggers

--- src/conversation.lua
+++ src/conversation.lua
@@ -2,8 +2,8 @@ local utils = require("utils")
 
 local conversation = {
     ["^%s*ping%s*$"] = "pong!",
-    ["^%s*乒%s*$"] = "乓！",
-    ["1%s-1%s-4%s-5%s-1%s-4"] = "sticker#CAADBQADLAAD1vXIAYjCdJop7aEIAg"
+    -- ["^%s*乒%s*$"] = "乓！",
+    -- ["1%s-1%s-4%s-5%s-1%s-4"] = "sticker#CAADBQADLAAD1vXIAYjCdJop7aEIAg"
 }
 
 return conversation
